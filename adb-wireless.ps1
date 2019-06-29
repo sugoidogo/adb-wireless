cdebf93978067817d25436979b667e33dc45c059
josephsmendoza <josephsmendoza@gmail.com>
2019-06-29 01:56:46 -0700

    it actually works now

--- adb-wireless.ps1
+++ adb-wireless.ps1
@@ -28,14 +28,16 @@ $devmodel=&$adb shell getprop ro.product.model
 if($shell.Popup("connect $devmodel $devid wirelessely?",0,'',4) -eq 6) {
 $tcpip=&$adb shell getprop service.adb.tcp.port
 if ($tcpip.Length -eq 0)
-{&$adb tcpip:5555
-$tcpip='5555'}
+{&$adb tcpip 5555
+$tcpip='5555'
+Start-Sleep 1
+&$adb wait-for-device}
 $ip=&$adb shell ip -4 addr show wlan0
 $ip=$deviceIPRegex.Match($ip)
 if($ip.Length -eq 0)
 {$shell.Popup("Device IP not found!")
 exit 1}
-$result=&adb connect $ip':'$tcpip
+$result=&$adb connect $ip':'$tcpip
 $shell.Popup($result)
 }
 }
